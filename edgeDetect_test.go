7d751e3716e2821de168c3c314062177ea51bd02
Nikolas M Skoufis <n.skoufis@gmail.com>
2015-06-15 17:24:24 +1000

    Created outline of edge detection tests
    These tests are run on a struct that has paths to the input and expected
    result along with the parameters used to get to that result
    
    Testing asserts that the edge detection image is only black or white
    pixels, that the dimensions of the input and the expected result match
    the actual result, and that the difference in individual pixels between
    the actual and expected is less than some delta.
    
    We sill need to actually write the methods that count the different
    pixels, as well as the method to make sure the dimentions are the same.

---
+++ edgeDetect_test.go
@@ -0,0 +1,71 @@
+package GoPixel
+
+import "testing"
+import "io"
+import "io/ioutil"
+import "github.com/niksko/GoPixel/edgeDetect"
+import "github.com/stretchr/testify/assert"
+
+func TestCannyEdgeDetection(t *testing.T) {
+	assert := assert.New(t)
+
+	// How many pixels different we're allowing between the actual and expected before we
+	// can't satisfythe assertion
+	const DELTA = 10
+
+	// Test a few different images to make sure that their edge detection results
+	// match the expected results from another source
+	cases := []struct {
+		inputFilePath, expectedFilePath            string
+		sigmaValue, lowerThreshold, upperThreshold float32
+	}{
+		{"foo", "bar", 0.1, 0.2, 0.3},
+	}
+	for _, c := range cases {
+		// We wrap each case in a function so that we can make use of defer for cleaning up
+		// the open files
+		func() {
+			// Read the input file
+			inFile, err := ioutil.ReadFile(c.inputFilePath)
+			defer inFile.Close()
+			// Make sure we found it and read it correctly
+			if !assert.Nil(err, "was able to oepn the input file") {
+				return
+			}
+
+			// Read the expected file
+			expectedFile, err := ioutil.ReadFile(c.expectedFilePath)
+			defer expectedFile.Close()
+			// Make sure we found it and read it correctly
+			if !assert.Nil(err, "was able to open the expected file") {
+				return
+			}
+
+			// Decode the input image
+			inputImage, _, err := image.Decode(inFile)
+			if !assert.Nil(err, "was able to decode the input file") {
+				return
+			}
+
+			// Decode the expected image
+			expectedImage, _, err := image.Decode(expectedFile)
+			if !assert.Nil(err, "Was able to decode the expected file") {
+				return
+			}
+
+			// Run the edge detection on the input file with the specified parameters and get the result
+			got := edgeDetect.CannyEdgeDetect(inputImage, c.sigmaValue, c.lowerThreshold, c.upperThreshold)
+			// Assert that the image consists of only black or white pixels
+			assert.True(t, isBlackAndWhite(got))
+
+			// Assert that tthe image is the same size as the original and the expected
+			assert.True(t, sameDimensions(got, inputImage))
+			assert.True(t, sameDimensions(got, expectedImage))
+
+			// Run the expected and actual images through a method that compares every pixel in the
+			// images, counting a 0 if the pixels are the same and a 1 otherwise, then returns the
+			// accumulated result. Then see whether this value is within some delta of zero
+			assert.InDelta(t, countDifferentPixels(expectedImage, got), 0, DELTA)
+		}()
+	}
+}
