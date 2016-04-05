9139f3b8e987b9f63896397eb901f000b9cc17f4
Nikolas Skoufis <n.skoufis@gmail.com>
2016-04-05 15:57:56 +1000

    Partial rewrite of line generating code

--- createPixelSegments.go
+++ createPixelSegments.go
@@ -3,42 +3,32 @@ package gopixel
 import "image"
 import "math"
 
+/*
+ * This function takes an image and a segment angle, and creates PixelSegments out of that image. This involves
+ * splitting the image up into lines of pixels with the gradient given by the segment angle.
+ */
 func createSegments(sourceImage image.Image, segmentAngle float64) []PixelSegment {
     var returnSlice []PixelSegment
 
     // Convert our segment angle into a gradient for our lines
     lineGradient := math.Tan(segmentAngle)
 
-    // We want to track the furthest right pixel that we visit. That way we can ensure that we assign every pixel in
-    // the image to a pixel segment
-    rightmostPixelVisited := 0
-
-    var startingY, increment, bound int
-
-    // Set up a starting pixel and an increment, depending on the gradient
-    if lineGradient > 0 || math.IsNaN(lineGradient){
-        startingY, bound := sourceImage.Bounds().Max.Y, 0
-    } else if lineGradient < 0 {
-        startingY, bound := 0, sourceImage.Bounds().Max.Y
+    // For positive gradients, start in the top left
+    if lineGradient >= 0 {
+        endPoint := image.Point{0, 0}
+        if lineGradient >= 1 {
+            endPoint.X += 1
+            endPoint.Y -= int(lineGradient+0.5) // Rounding, since apparently not in the standard library
+        } else {
+            endPoint.X += int:wq(1 / lineGradient + 0.5)
+        }
+    } else {
     }
 
-    // Iterate down the left hand size of the image
-    for y := startingY; y != bound; {
-        // Set up the segment with starting point
-        var segment PixelSegment
-        segment.startPoint = image.Point{0, y}
-        // Iterate over the pixels in the line determined by the lineGradient
-
-    }
+    return returnSlice
 }
 
-func createSegmentsWithEdgeMap(sourceImage image.Image, edgeMap image.Image, segmentAngle float32) []PixelSegment {
-}
-
-// This function takes a gradient and starting points and returns a function that generates points along the line 
-// defined by those values
-func pixelLine(gradient float64, startX int, startY int) func(int) int {
-    return func(xValue int) int {
-        yValue := 
-    }
+func createSegmentsWithEdgeMap(sourceImage image.Image, edgeMap image.Image, segmentAngle float64) []PixelSegment {
+    var returnSlice []PixelSegment
+    return returnSlice
 }
---
+++ createSegments_test.go
@@ -0,0 +1,40 @@
+package gopixel
+
+import "testing"
+import "image"
+import "os"
+import "math"
+import "bufio"
+import _ "image/png"
+
+var createSegmentTests = []struct {
+    angle float64
+    numberOfPixels int
+}{
+//    {45., 16},
+//    {0., 16},
+//    {90., 16},
+    {62., 16},
+    {17., 16},
+}
+
+func Test4x4(t *testing.T) {
+    for _, value := range createSegmentTests {
+        file, _ := os.Open("test/4x4.png")
+        angleRadians := float64(value.angle / 180. * math.Pi)
+        defer file.Close()
+        testImage, _, _ := image.Decode(bufio.NewReader(file))
+        pixelSegments := createSegments(testImage, angleRadians)
+        pixelCount := 0
+        for _, segment := range pixelSegments {
+            for range segment.pixelSlice{
+                pixelCount += 1
+            }
+        }
+        if pixelCount != value.numberOfPixels {
+            t.Errorf("Not enough pixels with angle %v\n", value.angle)
+            t.Errorf("Expected %v, got %v\n", value.numberOfPixels, pixelCount)
+            t.Errorf("%+v\n", pixelSegments)
+        }
+    }
+}
