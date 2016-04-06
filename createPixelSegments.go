4ee9aada741fa1e8c3181a2757a81424eef5dff5
Nikolas Skoufis <n.skoufis@gmail.com>
2016-04-06 11:14:16 +1000

    More partial fixes

--- createPixelSegments.go
+++ createPixelSegments.go
@@ -1,6 +1,7 @@
 package gopixel
 
 import "image"
+import "image/color"
 import "math"
 
 /*
@@ -19,21 +20,24 @@ func createSegments(sourceImage image.Image, segmentAngle float64) []PixelSegmen
         var startX, startY, endX, endY int
 
         // For positive gradients, our endpoint is at (0, 0) and our start point is on the line y=height
-        if lineGradient > 0 {
-            endX, endY = 0, 0
+        if lineGradient < 0 {
+            endX = 0
+            endY = 0
             // Compute start x and y points. Note that the start x needs manual rounding with the +0.5 and int cast
             startY = sourceImage.Bounds().Max.Y - 1
-            startX = -1 * int(float64(startY) / lineGradient + 0.5)
-        } else if lineGradient < 0 {
+            startX = int(float64(startY) / lineGradient - 0.5)
+        } else if lineGradient > 0 {
             // For negative gradients, the end point is at (0, height) and the start is at y=0
-            endX, endY = 0, sourceImage.Bounds().Max.Y - 1
-            startX = int(float64(endY) / lineGradient - 0.5)
+            endY = sourceImage.Bounds().Max.Y - 1
+            endX = 0
+            startX = int(-1 * float64(endY) / lineGradient - 0.5)
             startY = 0
         }
 
         // We need to loop until our start point reaches the correct value
         for ;startX < sourceImage.Bounds().Max.X; {
             var currentSegment PixelSegment
+            currentSegment.pixelSlice = []color.Color{}
 
             // We need to set the startPixel of the PixelSegment. To do this we set a flag which will be removed upon
             // The first pixel in bounds
@@ -44,35 +48,37 @@ func createSegments(sourceImage image.Image, segmentAngle float64) []PixelSegmen
             deltaY := float64(endY - startY)
             err := 0.
             deltaErr := math.Abs(deltaY / deltaX)
-            y := startY
-            for x := startX; x <= endX; x++ {
+            yVar := startY
+            for xVar := startX; xVar <= endX; xVar++ {
                err += deltaErr
                for ;err >= 0.5; {
                    // If the pixel is within the image bounds
-                   if inBounds(sourceImage, x, y) {
+                   if inBounds(sourceImage, xVar, yVar) {
                        // Write the pixel to the segment
-                       currentSegment.pixelSlice = append(currentSegment.pixelSlice, sourceImage.At(x, y))
+                       currentSegment.pixelSlice = append(currentSegment.pixelSlice, sourceImage.At(xVar, yVar))
                        // If this is the first inbounds pixel, write the point to the start point and remove the flag
                        if firstPixel {
                            firstPixel = false
-                           currentSegment.startPoint = image.Point{x, y}
+                           currentSegment.startPoint = image.Point{xVar, yVar}
                        }
                    }
                    // Adjust y based on sign of y1 - y0
                    if math.Signbit(float64(endY - startY)) {
                        // Negative sign
-                       y -= 1
+                       yVar -= 1
                    } else {
                        // Positive sign
-                       y += 1
+                       yVar += 1
                    }
                    // Adjust the error
                    err -= 1
                }
             }
 
-            // Add the segment to the returnSlice
-            returnSlice = append(returnSlice, currentSegment)
+            // Add the segment to the returnSlice, but only if it isn't empty
+            if len(currentSegment.pixelSlice) > 0 {
+                returnSlice = append(returnSlice, currentSegment)
+            }
 
             // Increment the x start and end points. This should ensure that we cover the entire image
             startX += 1
