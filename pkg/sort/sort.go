246fbb2e6672453e1dcd08873a0b40d54dc6cd01
Nikolas M Skoufis <n.skoufis@gmail.com>
2020-04-14 16:29:33 +1000

    Implement very basic sorting of pixels

--- pkg/sort/sort.go
+++ pkg/sort/sort.go
@@ -1,7 +1,47 @@
 package sort
 
-import "image"
+import (
+	"image"
+	"image/color"
+	s "sort"
+)
 
-func sort(img image.Image) image.Image {
-	return img
+func brightness(c color.Color) uint32 {
+	r, g, b, _ := c.RGBA()
+	return r + g + b
+}
+
+type ByBrightness []color.Color
+
+func (c ByBrightness) Len() int {
+	return len(c)
+}
+
+func (c ByBrightness) Swap(i, j int) {
+	c[i], c[j] = c[j], c[i]
+}
+
+func (c ByBrightness) Less(i, j int) bool {
+	return brightness(c[i]) > brightness(c[j])
+}
+
+func sort(inputImage image.Image) image.Image {
+	// Set up sorted image bounds
+	bounds := inputImage.Bounds()
+	imageRectangle := image.Rect(bounds.Min.X, bounds.Min.Y, bounds.Max.X, bounds.Max.Y)
+	sortedImage := image.NewRGBA(imageRectangle)
+
+	// Sort image, brightest pixels on the left
+	pixels := make([]color.Color, bounds.Dx())
+	for i := bounds.Min.X; i < bounds.Max.X; i++ {
+		pixels[i] = inputImage.At(i, bounds.Min.Y)
+	}
+
+	s.Sort(ByBrightness(pixels))
+
+	for i := bounds.Min.X; i < bounds.Max.X; i++ {
+		sortedImage.Set(i, bounds.Min.Y, pixels[i])
+	}
+
+	return sortedImage
 }
--- pkg/sort/sort_test.go
+++ pkg/sort/sort_test.go
@@ -8,6 +8,31 @@ import (
 	_ "image/png"
 )
 
+func compareImages(img1, img2 image.Image) bool {
+	if img1.Bounds() != img2.Bounds() {
+		return false
+	}
+	for i := img1.Bounds().Min.X; i < img1.Bounds().Max.X; i++ {
+		for j := img1.Bounds().Min.Y; j < img1.Bounds().Max.Y; j++ {
+			r1, g1, b1, a1 := img1.At(i, j).RGBA()
+			r2, g2, b2, a2 := img2.At(i, j).RGBA()
+			if r1 != r2 {
+				return false
+			}
+			if g1 != g2 {
+				return false
+			}
+			if b1 != b2 {
+				return false
+			}
+			if a1 != a2 {
+				return false
+			}
+		}
+	}
+	return true
+}
+
 func TestSort(t *testing.T) {
 	reader, err := os.Open("data/single-row.png")
 	if err != nil {
@@ -21,22 +46,22 @@ func TestSort(t *testing.T) {
 		t.Errorf("Failed to decode image with error: %s", err)
 	}
 
-	sortedImg := sort(img)
+	sortedImage := sort(img)
 
-	var sortedReader *os.File
-	sortedReader, err = os.Open("data/single-row-sorted.png")
+	var expectedReader *os.File
+	expectedReader, err = os.Open("data/single-row-sorted.png")
 	if err != nil {
 		t.Errorf("Failed to load result image")
 	}
-	defer sortedReader.Close()
+	defer expectedReader.Close()
 
-	var sortedImage image.Image
-	sortedImage, _, err = image.Decode(sortedReader)
+	var expectedImage image.Image
+	expectedImage, _, err = image.Decode(expectedReader)
 	if err != nil {
 		t.Errorf("Failed to decode result image with error: %s", err)
 	}
 
-	if sortedImage != sortedImg {
-		t.Errorf("Image didn't match expected image")
+	if !compareImages(sortedImage, expectedImage) {
+		t.Errorf("Image didn't match expected image\nExpected: %v+\nGot     : %v+", expectedImage, sortedImage)
 	}
 }
