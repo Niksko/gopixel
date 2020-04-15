3b15a737824fdacdd48e6f90ec600b7f140012fe
Nikolas M Skoufis <n.skoufis@gmail.com>
2020-04-15 12:50:36 +1000

    Add function to generate a point order, with test
    This adds a function (with a test) that generates what we call a 'point
    order'. This is an array of arrays of points. We should be able to use
    this both to generate arrays of arrays of colors (that can then be
    sorted), as well as to reconstruct the image when the points are sorted.

--- pkg/sort/sort.go
+++ pkg/sort/sort.go
@@ -25,13 +25,27 @@ func (c ByBrightness) Less(i, j int) bool {
 	return brightness(c[i]) > brightness(c[j])
 }
 
+func generatePointOrder(bounds image.Rectangle, sortAngle uint) [][]image.Point {
+	var pointOrder [][]image.Point
+	if sortAngle == 270 {
+		pointOrder = make([][]image.Point, bounds.Dy())
+		for y := bounds.Min.Y; y < bounds.Max.Y; y++ {
+			pointSegment := make([]image.Point, bounds.Dx())
+			for x := bounds.Min.X; x < bounds.Max.X; x++ {
+				pointSegment[x] = image.Pt(x, y)
+			}
+			pointOrder[y] = pointSegment
+		}
+	}
+	return pointOrder
+}
+
 func Sort(inputImage image.Image, sortAngle uint) image.Image {
 	// Set up sorted image bounds
 	bounds := inputImage.Bounds()
 	imageRectangle := image.Rect(bounds.Min.X, bounds.Min.Y, bounds.Max.X, bounds.Max.Y)
 	sortedImage := image.NewRGBA(imageRectangle)
 
-	// Sort image, brightest pixels on the left
 	pixels := make([][]color.Color, bounds.Dy())
 	for y := bounds.Min.Y; y < bounds.Max.Y; y++ {
 		pixelRow := make([]color.Color, bounds.Dx())
--- pkg/sort/sort_test.go
+++ pkg/sort/sort_test.go
@@ -34,6 +34,44 @@ func compareImages(img1, img2 image.Image) bool {
 	return true
 }
 
+func comparePointOrders(pointOrder1, pointOrder2 [][]image.Point) bool {
+	for i, points := range pointOrder1 {
+		for j, point1 := range points {
+			point2 := pointOrder2[i][j]
+			if point1.X != point2.X || point1.Y != point2.Y {
+				return false
+			}
+		}
+	}
+	return true
+}
+
+func TestGeneratePointOrder(t *testing.T) {
+	testCases := []struct {
+		bounds             image.Rectangle
+		sortAngle          uint
+		expectedPointOrder [][]image.Point
+	}{
+		{
+			image.Rect(0, 0, 4, 4),
+			270,
+			[][]image.Point{
+				[]image.Point{image.Pt(0, 0), image.Pt(1, 0), image.Pt(2, 0), image.Pt(3, 0)},
+				[]image.Point{image.Pt(0, 1), image.Pt(1, 1), image.Pt(2, 1), image.Pt(3, 1)},
+				[]image.Point{image.Pt(0, 2), image.Pt(1, 2), image.Pt(2, 2), image.Pt(3, 2)},
+				[]image.Point{image.Pt(0, 3), image.Pt(1, 3), image.Pt(2, 3), image.Pt(3, 3)},
+			},
+		},
+	}
+
+	for _, testCase := range testCases {
+		result := generatePointOrder(testCase.bounds, testCase.sortAngle)
+		if !comparePointOrders(result, testCase.expectedPointOrder) {
+			t.Fatalf("Test case didn't match.\nGot:      %v\nExpected: %v\n", result, testCase.expectedPointOrder)
+		}
+	}
+}
+
 func TestSort(t *testing.T) {
 	testCases := []struct {
 		inputPath string
