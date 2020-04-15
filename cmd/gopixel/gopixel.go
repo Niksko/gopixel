0f64607ea63616e165b4dbc1733e689410975aed
Nikolas M Skoufis <n.skoufis@gmail.com>
2020-04-15 11:13:30 +1000

    Add basic arg checking and usage instructions

--- cmd/gopixel/gopixel.go
+++ cmd/gopixel/gopixel.go
@@ -5,7 +5,16 @@ import (
 	"os"
 )
 
+func usage() {
+	os.Stdout.WriteString("Usage: gopixel <input-file> > <output-file>\n")
+}
+
 func main() {
+	if len(os.Args) != 2 {
+		os.Stderr.WriteString("Invalid number of arguments\n")
+		usage()
+		os.Exit(1)
+	}
 	filename := os.Args[1]
 	ok, _ := cmd.Sort(filename)
 	if ok {
